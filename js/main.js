fa651e238dfedae71c99515cef8aba0ea8798aba
OGI <t.ogi.1211@gmail.com>
2022-02-12 16:13:02 +0900

    set timeout to 3 sec

--- js/main.js
+++ js/main.js
@@ -24,20 +24,22 @@ const linkItem = (href, linkText, imageSrc, altText) => {
 }
 
 const promiseMock = new Promise((resolve) => {
-    resolve([
-        {
-            to: "bookmark.html",
-            img: "1.png",
-            alt: "画像1",
-            text: "ブックマーク"
-        },
-        {
-            to: "message.html",
-            img: "2.png",
-            alt: "画像2",
-            text: "メッセージ"
-        }
-    ])
+    setTimeout(() => {
+        resolve([
+            {
+                to: "bookmark.html",
+                img: "1.png",
+                alt: "画像1",
+                text: "ブックマーク"
+            },
+            {
+                to: "message.html",
+                img: "2.png",
+                alt: "画像2",
+                text: "メッセージ"
+            }
+        ])
+    }, 3000);
 });
 
 const ul = document.getElementById("app");
