017267d6a5a48251670f98a2e503a93b7ab65fe6
OGI <t.ogi.1211@gmail.com>
2022-02-16 23:18:33 +0900

    fix: adjusted path to image dir

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // build ul > li*2
 const ul = document.createElement("ul");
-ul.appendChild(listItem("a1.html", "a1", "/img/bookmark.png"));
-ul.appendChild(listItem("a2.html", "a2", "/img/message.png"));
+ul.appendChild(listItem("a1.html", "a1", "/image/bookmark.png"));
+ul.appendChild(listItem("a2.html", "a2", "/image/message.png"));
 
 // append list to app
 document.getElementById("app")
